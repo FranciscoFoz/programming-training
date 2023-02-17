a4fba2a13af4da6e601f497954a531364a25cc7d
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-02-17 09:39:22 -0300

    Inserindo soma_numeros.py

--- 1 - Tipos numéricos/soma_numeros.py
+++ 1 - Tipos numéricos/soma_numeros.py
@@ -1,1 +1,20 @@
 #SOMA DE NÚMEROS
+'''
+Esta função irá receber uma quantidade variável de números, realizará a soma deles e retornará 
+o resultado final.
+
+'''
+
+def soma(*numeros):
+
+    resultado = 0
+
+    for i in numeros:
+        resultado += i
+
+    return resultado
+
+
+
+soma(10,20,5,5,10,*[25,25])
+
