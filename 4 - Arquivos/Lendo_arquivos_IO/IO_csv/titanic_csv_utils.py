c44ef9ff4b03c7f35f85a0c6d8f1fa5f52465e2c
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-02-27 08:18:35 -0300

    Modificando importação correta

--- 4 - Arquivos/Lendo_arquivos_IO/IO_csv/titanic_csv_utils.py
+++ 4 - Arquivos/Lendo_arquivos_IO/IO_csv/titanic_csv_utils.py
@@ -16,22 +16,5 @@ def csv_para_titanic_csv(caminho, encoding):
 
     return linhas
 
-class SpaceshipTitanic:
-    
-    def __init__(self,PassengerId,HomePlanet,CryoSleep,Cabin,Destination,Age,VIP,RoomService,FoodCourt,ShoppingMall,Spa,VRDeck,Name,Transported):
-        self.PassengerId = PassengerId
-        self.HomePlanet = HomePlanet
-        self.CryoSleep = CryoSleep
-        self.Cabin = Cabin
-        self.Destination = Destination
-        self.Age = Age
-        self.VIP = VIP
-        self.RoomService = RoomService
-        self.FoodCourt = FoodCourt
-        self.ShoppingMall = ShoppingMall
-        self.Spa = Spa
-        self.VRDeck = VRDeck
-        self.Name = Name 
-        self.Transported = Transported
 
 csv_para_titanic_csv('4 - Arquivos/Lendo_arquivos_IO/IO_csv/spaceship_titanic_train.csv',encoding='latin_1')
