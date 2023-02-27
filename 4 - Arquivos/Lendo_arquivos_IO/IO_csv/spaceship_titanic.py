780ba64c7c38662e3d9a0bb68e89b062c5087419
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-02-27 08:09:53 -0300

    Inserindo titanic_csv.utils

---
+++ 4 - Arquivos/Lendo_arquivos_IO/IO_csv/spaceship_titanic.py
@@ -0,0 +1,20 @@
+class SpaceshipTitanic:
+    
+    def __init__(self,PassengerId,HomePlanet,CryoSleep,Cabin,Destination,Age,VIP,RoomService,FoodCourt,ShoppingMall,Spa,VRDeck,Name,Transported):
+        self.PassengerId = PassengerId
+        self.HomePlanet = HomePlanet
+        self.CryoSleep = CryoSleep
+        self.Cabin = Cabin
+        self.Destination = Destination
+        self.Age = Age
+        self.VIP = VIP
+        self.RoomService = RoomService
+        self.FoodCourt = FoodCourt
+        self.ShoppingMall = ShoppingMall
+        self.Spa = Spa
+        self.VRDeck = VRDeck
+        self.Name = Name 
+        self.Transported = Transported
+
+
+
