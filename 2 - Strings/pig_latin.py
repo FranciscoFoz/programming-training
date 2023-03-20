e2340b3e596283ed555e1e4301c2128b208db2a5
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-03-20 06:29:14 -0300

    Finalizando pig_latin.py

--- 2 - Strings/pig_latin.py
+++ 2 - Strings/pig_latin.py
@@ -6,15 +6,24 @@ Se não, irá colocar a primeira letra dela ao final e incluir a palavra "ay".
 '''
 
 def pig_latin(word):
-    if word[0] in 'aeiou':
-        return f'{word}way'
+           
+    if word[1] in 'aeiouAEIOU':
+        if word[-1].isupper():
+            return f'{word}WAY'
+        else:
+            return f'{word}way'
+    
+    if word[0] in 'aeiouAEIOU':
+        if word[-1].isupper():
+            return f'{word}WAY'
+        else:
+            return f'{word}way'
+
 
     return f'{word[1:]}{word[0]}ay'
 
-palavras  = ['eat','car','computer','love','peace','hippie']
+palavras  = ['eat','car','computer','love','peace','hippie','WINE','Wind','YESTERDAY']
 
 for palavra in palavras:
     print(pig_latin(palavra))
-
-
 
