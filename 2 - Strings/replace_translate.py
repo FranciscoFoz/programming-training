2e64a9254a33fab373ea4f07bb2cda204caec233
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-04-03 06:50:35 -0300

    Iniciando replace_translate

---
+++ 2 - Strings/replace_translate.py
@@ -0,0 +1,22 @@
+#C
+'''
+Essa função irá receber uma palavra e traduzir ela para Ubby Dubbi.
+Ela irá verificar se cada string da palavra possuiu uma vogal e irá 
+substituí-la pelo conjunto "ub".
+Retornando a palavra modificado como output.
+'''
+
+def replace(palavra):
+    for vogal in 'aeiouAEIOU':
+        palavra = palavra.replace(vogal, 'x')
+        
+    return palavra
+
+palavras  = ['eat','car','computer','love','peace','hippie','WINE','Wind','YESTERDAY','test']
+
+for palavra in palavras:
+    print(replace(palavra))
+
+#TRANSLATE
+#FOR 
+# - > COMPARAR TEMPO
