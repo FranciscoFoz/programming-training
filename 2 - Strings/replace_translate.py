6eea0e605f58871b245cbcd0ed49901c894370d8
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-04-10 06:17:21 -0300

    Adicionando replace_translate

--- 2 - Strings/replace_translate.py
+++ 2 - Strings/replace_translate.py
@@ -1,10 +1,10 @@
-#C
+#REPLACE TRANSLATE
 '''
-Essa função irá receber uma palavra e traduzir ela para Ubby Dubbi.
-Ela irá verificar se cada string da palavra possuiu uma vogal e irá 
-substituí-la pelo conjunto "ub".
-Retornando a palavra modificado como output.
+Esse código irá testar o tempo de funções que trocam vogais pela letra x, utilizando replace(), 
+maketrans()/tranlate() e um for simples. 
 '''
+import time
+import numpy as np
 
 def replace(palavra):
     for vogal in 'aeiouAEIOU':
@@ -12,13 +12,55 @@ def replace(palavra):
         
     return palavra
 
+def translate_maketrans(palavra):
+    vogais = 'aeiouAEIOU'
+    trans_table = str.maketrans(vogais, 'x'*len(vogais))
+    return palavra.translate(trans_table)
+
+def replace_for(palavra):
+    nova_palavra = ''
+    for letra in palavra:
+        if letra in 'aeiouAEIOU':
+            nova_palavra += 'x'
+        else:
+            nova_palavra += letra
+    return nova_palavra
+
 palavras  = ['eat','car','computer','love','peace','hippie','WINE','Wind','YESTERDAY','test']
+palavras_aleatorias = np.random.choice(palavras,size=1000)
 
-for palavra in palavras:
-    print(replace(palavra))
 
-#TRANSLATE
-#FOR 
-# - > COMPARAR TEMPO
+#Teste tempo replace
+inicio = time.time()
+lista_1 = []
+for palavra in palavras_aleatorias:
+    replace(palavra).append(lista_1)
+fim = time.time()
+tempo_replace = fim - inicio
 
-#Cifra césar
+#Teste tempo translate_maketrans
+inicio_2 = time.time()
+lista_2 = []
+for palavra in palavras_aleatorias:
+    translate_maketrans(palavra).append(lista_2)
+fim_2 = time.time()
+tempo_maketrans = fim_2 - inicio_2
+
+#Teste tempo translate_maketrans
+inicio_2 = time.time()
+lista_2 = []
+for palavra in palavras_aleatorias:
+    translate_maketrans(palavra).append(lista_2)
+fim_2 = time.time()
+tempo_maketrans = fim_2 - inicio_2
+
+#Teste tempo replace_for
+inicio_3 = time.time()
+lista_3 = []
+for palavra in palavras_aleatorias:
+    replace_for(palavra).append(lista_3)
+fim_3 = time.time()
+tempo_replace_for = fim_3 - inicio_3
+
+
+print(f'Tempo replace:{tempo_replace}\nTempo maketrans: {tempo_maketrans}\nTempo replace_for: {tempo_replace_for}')
