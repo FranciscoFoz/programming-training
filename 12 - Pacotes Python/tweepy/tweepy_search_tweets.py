cbb791cc123846634cccfb9f2d4ac3aa73b2d374
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-27 15:15:58 -0300

    Ajusdatando fuso horário código

--- 12 - Pacotes Python/tweepy/tweepy_search_tweets.py
+++ 12 - Pacotes Python/tweepy/tweepy_search_tweets.py
@@ -13,52 +13,34 @@ client = tweepy.Client(bearer_token)
 
 string_de_busca = 'biblioteconomia -is:retweet'
 
-resultados_por_requisicao = 30  
+tweets = client.search_recent_tweets(
+    string_de_busca,
+    expansions=['author_id'],
+    tweet_fields=['created_at'],
+    max_results=10
+)
+
 total_tweets = 0
 
-datas_ultimos_tweets = []
+for tweet in tweets.data:
+    tweet_datetime = datetime.strptime(str(tweet.created_at), "%Y-%m-%d %H:%M:%S%z")
+    brasilia_offset = timedelta(hours=-3)
+    
+    tweet_datetime_brasilia = tweet_datetime + brasilia_offset
+    
+    # Obter o nome do usuário
+    users = client.get_user(id=tweet.author_id).data
+    print(f'USERNAME: @{users}')
+    print(f'NOME: {users.name}')
+    print(f'Data/Hora - Brasília: {tweet_datetime_brasilia.date()} {tweet_datetime_brasilia.time()}')
+    print(f'TWEET ID: {tweet.id}')
+    print(f'TWEET: {tweet.text}')
+    print(f'Nº TWEET: {total_tweets + 1}')
+    print(f'Data/Hora - GMT: {tweet_datetime.date()} {tweet_datetime.time()}')
+    print('-' * 30)
+    
+    total_tweets += 1
+    
 
-while total_tweets < 30:
-    tweets = client.search_recent_tweets(
-        string_de_busca,
-        expansions=['author_id'],
-        tweet_fields=['created_at'],
-        max_results=resultados_por_requisicao
-    )
+# Data da API do Twitter em UTC
 
-    for tweet in tweets.data:
-        # Obter o nome do usuário
-        users = client.get_user(id=tweet.author_id).data
-
-        print(f'USERNAME: {users}')
-        print(f'Data/Hora: {tweet.created_at.date()} {tweet.created_at.time()}')
-        print(f'TWEET ID: {tweet.id}')
-        print(f'TWEET: {tweet.text}')
-        print(f'Nº TWEET: {total_tweets + 1}')
-
-        print(f'DATA: {tweet.created_at}')
-        print('-' * 30)
-
-        datas_ultimos_tweets.append(tweet.created_at)  
-
-        total_tweets += 1
-
-        if total_tweets >= 30:
-            break
-
-    # Obtém a data do último tweet da lista para usar como ponto de partida para a próxima requisição
-    data_ultimos_tweet = min(datas_ultimos_tweets)
-
-    data_minima = data_ultimos_tweet + timedelta(seconds=1)
-
-    # Define os parâmetros para buscar os próximos tweets com base na última data
-    params = {
-        "query": "Biblioteconomia",
-        "expansions": ["author_id"],
-        "tweet_fields": ["created_at"],
-        "max_results": resultados_por_requisicao,
-        "end_time": data_minima.isoformat()
-    }
-
-    # Realiza a próxima requisição de tweets
-    tweets = client.search_recent_tweets(**params)
