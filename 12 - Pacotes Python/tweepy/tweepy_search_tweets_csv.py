560f300bfa97c7606eddad79803fcd35432f9088
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-27 15:16:34 -0300

    Ajustando comentários

--- 12 - Pacotes Python/tweepy/tweepy_search_tweets_csv.py
+++ 12 - Pacotes Python/tweepy/tweepy_search_tweets_csv.py
@@ -11,7 +11,56 @@ with open('/home/franciscofoz/Documents/credentials_twitter.json') as arquivo:
 bearer_token = credenciais['bearer_token']
 client = tweepy.Client(bearer_token)
 
-user_id = 914378883763441664
+resultados_por_requisicao = 30  
+total_tweets = 0
+
+datas_ultimos_tweets = []
+
+while total_tweets < 30:
+    tweets = client.search_recent_tweets(
+        string_de_busca,
+        expansions=['author_id'],
+        tweet_fields=['created_at'],
+        max_results=resultados_por_requisicao
+    )
+
+    for tweet in tweets.data:
+        # Obter o nome do usuário
+        users = client.get_user(id=tweet.author_id).data
+
+        print(f'USERNAME: {users}')
+        print(f'Data/Hora: {tweet.created_at.date()} {tweet.created_at.time()}')
+        print(f'TWEET ID: {tweet.id}')
+        print(f'TWEET: {tweet.text}')
+        print(f'Nº TWEET: {total_tweets + 1}')
+
+        print(f'DATA: {tweet.created_at}')
+        print('-' * 30)
+
+        datas_ultimos_tweets.append(tweet.created_at)  
+
+        total_tweets += 1
+
+        if total_tweets >= 30:
+            break
+
+    # Obtém a data do último tweet da lista para usar como ponto de partida para a próxima requisição
+    data_ultimos_tweet = min(datas_ultimos_tweets)
+
+    data_minima = data_ultimos_tweet + timedelta(seconds=1)
+
+    # Define os parâmetros para buscar os próximos tweets com base na última data
+    params = {
+        "query": "Biblioteconomia",
+        "expansions": ["author_id"],
+        "tweet_fields": ["created_at"],
+        "max_results": resultados_por_requisicao,
+        "end_time": data_minima.isoformat()
+    }
+
+    # Realiza a próxima requisição de tweets
+    tweets = client.search_recent_tweets(**params)
+
 
 string_de_busca = 'biblioteconomia'
 
@@ -20,7 +69,7 @@ total_tweets = 0
 
 datas_ultimos_tweets = []
 
-while total_tweets < 200:
+while total_tweets < 100:
     tweets = client.search_recent_tweets(
         string_de_busca,
         expansions=['author_id'],
@@ -46,7 +95,7 @@ while total_tweets < 200:
         
         total_tweets += 1
 
-        if total_tweets >= 200:
+        if total_tweets >= 100:
             break
 
     # Obtém a data do último tweet da lista para usar como ponto de partida para a próxima requisição
@@ -56,7 +105,7 @@ while total_tweets < 200:
 
     # Define os parâmetros para buscar os próximos tweets com base na última data
     params = {
-        "query": "string_de_busca",
+        "query": string_de_busca,
         "expansions": ["author_id"],
         "tweet_fields": ["created_at"],
         "max_results": resultados_por_requisicao,
