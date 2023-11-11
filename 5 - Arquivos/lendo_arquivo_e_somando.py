2251b2bf4cd4d7d432b265370ca4811d7acd7b94
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-11 12:16:57 -0300

    Finalizando lendo_arquivo_e_somando.py

--- 5 - Arquivos/lendo_arquivo_e_somando.py
+++ 5 - Arquivos/lendo_arquivo_e_somando.py
@@ -4,6 +4,14 @@
 Leia o e-mail ('text_sample_2_with_numbers.txt') e some a receita total.
 '''
 
+soma_receita_total = 0
+
 with open('/home/franciscofoz/Documents/GitHub/python-training/5 - Arquivos/text_sample_2_with_numbers.txt') as t:
     for linha in t.readlines():
-        print(linha)
+        if 'Receita total' in linha:
+            valor = linha.strip().split(' ')[-1].replace('$','').replace(',','')
+            soma_receita_total += int(valor)
+            
+soma_receita_total_formatada = "R$ {:,.2f}".format(soma_receita_total).replace(',','.')[::-1].replace('.',',',1)[::-1]
+            
+print(f'RECEITA TOTAL = {soma_receita_total_formatada}')
