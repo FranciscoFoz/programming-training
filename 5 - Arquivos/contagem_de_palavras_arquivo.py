ae4106149749bbf9cee295b83775c53072233ae3
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-12 20:09:14 -0300

    Finalizando contagem_de_palavras_arquivo.py

--- 5 - Arquivos/contagem_de_palavras_arquivo.py
+++ 5 - Arquivos/contagem_de_palavras_arquivo.py
@@ -2,15 +2,17 @@
 
 contagem = {'linhas':0,'caracteres':0,'palavras':0,'palavras_unicas':0}
 
+palavras_unicas = set()
+
 with open('arquivo_contagem_de_palavras.txt') as arquivo:
     for linha in arquivo:
-        print(linha,linha.split())
+        palavras_unicas.update(linha.split())
         contagem['linhas'] += 1
         contagem['caracteres'] += len(linha)
         contagem['palavras'] += len(linha.split())
-        contagem['palavras'] += len(linha.split())
-
+        
+    contagem['palavras_unicas'] = len(palavras_unicas)
+        
 print(contagem)
 
 
-# COLOCAR CONTAGEM DE PALAVRAS ÚNICAS
