e279b89fe002cc6b0c2d457bf8a745d24baa4b84
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-12 16:06:28 -0300

    Finalizando passwd_to_dict.py

--- 5 - Arquivos/passwd_to_dict.py
+++ 5 - Arquivos/passwd_to_dict.py
@@ -1,5 +1,10 @@
 # PASSWD TO DICT
 
+user_id = dict()
 with open('text_etc-passwd_sample.txt',encoding='utf-8') as text:
     for line in text.readlines():
-        print(line.strip())
+        if not line.startswith('#'):
+            linha_separada = line.strip().split(':')
+            user_id[linha_separada[0]] = linha_separada[2]
+            
+print(user_id)
