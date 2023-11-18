8876904b2f05480d9fdb85398a25f8cdc4948d06
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-18 20:57:47 -0300

    Finalizando palavra mais longa.py

--- 5 - Arquivos/palavra_mais_longa_arquivo.py
+++ 5 - Arquivos/palavra_mais_longa_arquivo.py
@@ -2,5 +2,24 @@
 
 import os
 
-with open('palavra_mais_longa_arquivo.txt') as arquivo:
-    for linha in arquivo:
+def palavra_mais_longa(caminho):
+    with open(f'{caminho}') as arquivo:
+        palavra_mais_longa = ''
+        for linha in arquivo:
+            for palavra in linha.split():
+                if len(palavra) > len(palavra_mais_longa):
+                    palavra_mais_longa = palavra
+        
+        return palavra_mais_longa
+    
+
+
+def procurar_dir_palavra_mais_longa(nome_diretorio):    
+    dicionario_palavras =  {filename:palavra_mais_longa(os.path.join(nome_diretorio,filename))
+                            for filename in os.listdir(nome_diretorio)
+                                if os.path.isfile(os.path.join(nome_diretorio,filename))}
+    print(dicionario_palavras)
+    
+    
+
+procurar_dir_palavra_mais_longa('sample_files_palavra_mais_longa')
