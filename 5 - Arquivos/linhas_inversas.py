f7a6dc39eb28f3b05ed1f38cc4e4594807a85176
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-23 22:47:10 -0300

    Finalizando linhas_inversas.py

--- 5 - Arquivos/linhas_inversas.py
+++ 5 - Arquivos/linhas_inversas.py
@@ -1,2 +1,8 @@
 # LINHAS INVERSAS
 
+def linhas_inversas(input, output):
+    with open(input) as infile, open(output, 'w') as outfile:
+        for one_line in infile:
+            outfile.write(f'{one_line.rstrip()[::-1]}\n')
+            
+linhas_inversas('arquivo_linhas_inversas.txt','arquivo_linhas_inversas_convertido.txt')
