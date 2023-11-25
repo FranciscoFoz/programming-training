7eddfcc3e729b060ef1917ef02575eab818fe342
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-11-24 22:35:53 -0300

    Subindo XML_generator

---
+++ 6 - Funções/4_XML_generator.py
@@ -0,0 +1,8 @@
+#XML GENERATOR
+
+def myxml(tagname, content='', **kwargs):
+    attrs = ''.join([f' {key}="{value}"' for key, value in kwargs.items()])
+        
+    return f'<{tagname}{attrs}>{content}</{tagname}>'
+
+print(myxml('tagname', 'hello', a=1, b=2, c=3))
