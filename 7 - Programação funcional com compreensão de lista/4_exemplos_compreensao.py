f1c412ebd3f67834363bbb4ac95ad0cfbf1ebd2e
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-12-06 07:17:51 -0300

    Subindo exemplos de compreesao

---
+++ 7 - Programação funcional com compreensão de lista/4_exemplos_compreensao.py
@@ -0,0 +1,12 @@
+# EXEMPLOS DE COMPREENSÃO
+
+lista = [x*x for x in range(5)]
+conjunto = {x*x for x in range(5)}
+dicionario = {x : 2*x for x in range(5)}
+
+print(lista)
+print(conjunto)
+print(dicionario)
+
+
+input('Name: ')
