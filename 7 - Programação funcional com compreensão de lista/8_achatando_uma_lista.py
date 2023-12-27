64f7088acc31627f3254391c8b68cb11aca791bb
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-12-27 07:11:01 -0300

    Ajustando 8_achatando_lista

--- 7 - Programação funcional com compreensão de lista/8_achatando_uma_lista.py
+++ 7 - Programação funcional com compreensão de lista/8_achatando_uma_lista.py
@@ -5,6 +5,7 @@ def achatando_lista(minha_lista):
         um_elemento
         for uma_sublista in minha_lista
         for um_elemento in uma_sublista
+        if um_elemento % 2 ==1
         ]
     
 print(achatando_lista([[1,2], [3,4],[5,6,7]]))
