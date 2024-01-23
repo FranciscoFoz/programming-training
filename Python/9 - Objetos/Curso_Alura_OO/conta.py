4470c5b35ebf6e472ed29cce53e63a791e0fe655
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2024-01-23 07:01:25 -0300

    Continuando curso Alura

--- Python/9 - Objetos/Curso_Alura_OO/conta.py
+++ Python/9 - Objetos/Curso_Alura_OO/conta.py
@@ -1,4 +1,18 @@
 
+class Conta:
+    
+    def __init__(self,numero, titular, saldo, limite):
+        print('Construindo objeto... {}'.format(self))
+        
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+conta = Conta(123,'Francisco',55.5,1000.0)
+conta.saldo
+
+conta2 = Conta(124,'Tadeu',65.0,1000.0)
 
 def cria_conta(numero, titular, saldo, limite):
    conta = {'numero': numero, 'titular': titular, 'saldo': saldo, 'limite': limite}
