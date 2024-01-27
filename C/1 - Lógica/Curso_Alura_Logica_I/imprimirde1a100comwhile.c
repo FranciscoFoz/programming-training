ce392d2aed9e21a1e9a1efff5f4bab118863cc10
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2024-01-27 17:18:41 -0300

    Subindo imprimirde1a100comwhile.c

---
+++ C/1 - Lógica/Curso_Alura_Logica_I/imprimirde1a100comwhile.c
@@ -0,0 +1,15 @@
+//IMPRIMINDO DE 1 A 100 COM WHILE
+
+#include <stdio.h>
+
+int main(){
+
+    int numero = 0;
+
+    while(numero <= 100){
+        printf("%d\n", numero);
+        numero++;
+
+    }
+
+}
