c5216dd2fdcf4e40b5d063fda25fcf0d40ded7ea
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2024-01-27 17:13:16 -0300

    Continuando curso C

--- C/1 - Lógica/Curso_Alura_Logica_I/adivinhacao.c
+++ C/1 - Lógica/Curso_Alura_Logica_I/adivinhacao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
 
 int main() {
 
@@ -10,13 +11,64 @@ int main() {
 
 
     int numerosecreto = 42;
-
     int chute;
+    int ganhou = 0;
+    int tentativas = 1;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n",chute);
-}
+    char frasemotivacional[] = "Mas não desanime. Tente novamente.\n\n";
+
+
+    while(1){
+    //for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
+        printf("+++++++++Tentativa %d++++++++++\n",tentativas);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n",chute);
+
+
+
+        int acertou = (chute == numerosecreto);
+        int maior = chute > numerosecreto;
+
+
+
+        if (chute < 0){
+            printf("Você não pode chutar números negativos!\n");
+            tentativas--;
+            
+            continue;
+
+        }
+
+
+        if(acertou){
+            printf("Parabéns! Você acertou!\n");
+
+            break;
+        }
+
+        else if(maior){
+            printf("Você errou! :( \n");
+            printf("Seu chute foi maior que o numero secreto.\n");
+
+            printf("%s", frasemotivacional);
+            }
+
+        else {
+            printf("Você errou! :( \n");
+            printf("Seu chute foi menor que o numero secreto. \n");
+
+            printf("%s", frasemotivacional);
+            }
+
+
+        tentativas++;
+        }
+            
+         
+    printf("++++++FIM DE JOGO+++++++\n\n");   
+    }
+
 
 
 
