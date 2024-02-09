0fd22a83e54c4c062fd339c311398306fa58e976
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2024-02-08 22:02:34 -0300

    Finalizando match_classifica_idades.py

--- Python/2 - Strings/match_classifica_idades.py
+++ Python/2 - Strings/match_classifica_idades.py
@@ -1,6 +1,6 @@
 # MATCH - CLASSIFICAR IDADES
 
-idade = int(input('Qual a sua idade?'))
+idade = input('Qual a sua idade?')
 
 match idade:
     case 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11:
